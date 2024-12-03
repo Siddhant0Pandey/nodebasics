adee60ba5e6f4ee62f12c33141b893f6f079359c
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-03 12:19:17 +0545

    simple crud api initialized

--- expressdemo/router.js
+++ expressdemo/router.js
@@ -4,6 +4,9 @@ const port = 4000;
 import userRouter from "./routes/user.js";
 // const userRouter = require("./routes/user");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/user", userRouter);
 
 app.listen(port, () => {
--- expressdemo/routes/user.js
+++ expressdemo/routes/user.js
@@ -2,12 +2,24 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Von" },
+  { id: 3, name: "Neuman" },
+  { id: 4, name: "SOn" },
+];
+
 router.get("/", (req, res) => {
   res.send("this is user page");
 });
 
 router.post("/", (req, res) => {
   res.send("user page created");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  //   res.status(201).json(users);
 });
 
 export default router;
